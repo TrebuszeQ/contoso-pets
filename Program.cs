0d050c997aef21c16923550c2d058b7e307f92ce
trebuszeq <trebuszeq@gmail.com>
2023-09-07 18:33:15 +0200

    7.08.23

--- Program.cs
+++ Program.cs
@@ -17,46 +17,38 @@ string age = "";
 string condition = "";
 string personality = "";
 string nickname = "";
+string suggestedDonation = "";
 
 int col = 10;
-int row = 6;
+int row = 7;
 string[,] ourAnimals = new string[col,row];
-// string animalSpecies = "";
-// string animalID = "";
-// string animalAge = "";
-// string animalPhysicalDescription = "";
-// string animalPersonalityDescription = "";
-// string animalNickname = "";
-//
-// // variables that support data entry
-// int maxPets = 8;
-// string? readResult;
-// string menuSelection = "";
-//
-// // array used to store runtime data, there is no persisted data
-// string[,] ourAnimals = new string[maxPets, 6];
-//
-
-
+string[] menuAct = { "List all pets in total.",
+    "List pets by specimen.",
+    "List each pet with details.",
+    "Find all dogs with specified characteristics.",
+    "Check if specified animal's age and physical description are complete.",
+    "Check if specified animal's nickname and personality description are complete.",
+    "Alter specified animal's age.",
+    "Alter animal's personality description.",
+    "Add animal to the list.",
+    "Exit",
+}; 
 
 void Main()
 {
     string? user_input;
     int? int_input;
-    add_zog();
+    
+    fill_with_examples();
     menu:
-    Console.WriteLine("\nWelcome to Contoso Pets.\n");
-    Console.WriteLine(@"Enter number of the option:
-    1. Print animals in total.
-    2. List found animals by species.
-    3. List each animal with details.
-    4. Check if specified animal's age and physical description are complete.
-    5. Check if specified animal's nickname and personality description are complete.
-    6. Alter specified animal's age.
-    7. Alter animal's personality description.
-    8. Add animal to the list.
-    9. Exit.
-    ");
+    Console.WriteLine("Welcome to Contoso Pet app.");
+    ushort counter = 1;
+    foreach (string option in menuAct)
+    {
+        Console.WriteLine($"{counter}. {option}");
+        counter++;
+    }
+    
     do
     {
         user_input = Console.ReadLine();
@@ -75,21 +67,25 @@ void Main()
                 list_w_details();
                 break;
             case 4:
-                are_age_cond_compl();
+                
                 break;
             case 5:
-                are_nick_pers_compl();
+                are_age_cond_compl();
                 break;
             case 6:
-                edit_age();
+                are_nick_pers_compl();
                 break;
             case 7:
-                edit_pers();
+                edit_age();
                 break;
             case 8:
-                case8();
-                goto menu;
+                edit_pers();
+                break;
             case 9:
+                int int_pos = choose_entry(false);
+                if (overwrite_entry(int_pos)) write_pet_data(int_pos);
+                goto menu;
+            case 10:
                 Console.WriteLine("Exiting\n");
                 return;
             default:
@@ -102,20 +98,71 @@ void Main()
 
 
 // adds first entry of array for testing purposes
-void add_zog()
+void fill_with_examples()
 {
-    string ID = "0";
-    string species = "dog";
-    string age = "5y";
-    string condition = "healthy";
-    string personality = "mild";
-    string nickname = "Zog";
-    ourAnimals[0, 0] = ID;
-    ourAnimals[0, 1] = species;
-    ourAnimals[0, 2] = age;
-    ourAnimals[0, 3] = condition;
-    ourAnimals[0, 4] = personality;
-    ourAnimals[0, 5] = nickname;
+    for (int i = 0; i < row; i++)
+    {
+        switch (i)
+        {
+            case 0:
+                ID = 1.ToString();
+                species = "dog";
+                age = "2";
+                condition = "medium sized cream colored female golden retriever weighing about 45 pounds. housebroken.";
+                personality = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
+                nickname = "lola";
+                suggestedDonation = "";
+                break;
+
+            case 1:
+                ID = 2.ToString();
+                species = "dog";
+                age = "9";
+                condition = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
+                personality = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
+                nickname = "gus";
+                suggestedDonation = "";
+                break;
+            
+            case 2:
+                ID = 3.ToString();
+                species = "cat";
+                age = "1";
+                condition = "small white female weighing about 8 pounds. litter box trained.";
+                personality = "friendly";
+                nickname = "snow";
+                suggestedDonation = "";
+                break;
+
+            case 3:
+                ID = 4.ToString();
+                species = "cat";
+                age = "3";
+                condition = "Medium sized, long hair, yellow, female, about 10 pounds. Uses litter box.";
+                personality = "A people loving cat that likes to sit on your lap.";
+                nickname = "Lion";
+                suggestedDonation = "";
+                break;
+            
+            default:
+                ID = "";
+                species = "";
+                age = "";
+                condition = "";
+                personality = "";
+                nickname = "";
+                suggestedDonation = "";
+                break;
+        }
+
+        ourAnimals[i, 0] = "ID #: " + ID;
+        ourAnimals[i, 1] = "Species: " + species;
+        ourAnimals[i, 2] = "Age: " + age;
+        ourAnimals[i, 3] = "Physical description: " + condition;
+        ourAnimals[i, 4] = "Personality: " + personality;
+        ourAnimals[i, 5] = "Nickname: " + nickname;
+        ourAnimals[i, 6] = "Suggested donation: " + $"{suggestedDonation:C}\n";
+    }
 }
 
 
@@ -173,7 +220,7 @@ void list_w_details()
             counter++;
         }
     }
-    if(counter == 0) Console.WriteLine("No animals found.\n");
+    if(counter == 0) Console.WriteLine("No pets found.\n");
 }
 
 
@@ -188,24 +235,20 @@ void list_entry(int pos)
         if(animal != null && animal.Length > 0) temp += $"{animal}\n";
     }
     if(temp != null && temp.Length > 0) Console.WriteLine(temp);
-    else Console.WriteLine("Entry is empty.\n");
 }
 
 
-// Controls main switch case no. 8 executing different methods to eventually write animal entry to the array. 
-void case8()
+// Lists specified entry and returns decision if user wants to overwrite it.
+bool overwrite_entry(int pos)
 {
-    string str_pos = "";
-    int int_pos = choose_entry(true);
-    list_entry(int_pos);
+    list_entry(pos);
     Console.WriteLine("Do you want to overwrite that entry?\n");
     bool decision = message_loop();
-    if (decision) write_specimen_data(int_pos);
+    return decision;
 }
 
-
-// reads user input and writes specimen data to array
-void write_specimen_data(int pos)
+// reads user input and writes specimen data to an array
+void write_pet_data(int pos)
 {
     for (int i = 0; i < 6; i++)
     {
@@ -213,38 +256,45 @@ void write_specimen_data(int pos)
         {
             case 0:
                 ID = pos.ToString();
-                ourAnimals[pos, i] = $"ID:\n{ID}\n";
+                ourAnimals[pos, i] = $"ID #: {ID+1}\n";
                 break;
             case 1:
                 Console.WriteLine("Enter specimen.");
                 species = Console.ReadLine();
-                ourAnimals[pos, i] = $"Specimen:\n{species}\n";
+                ourAnimals[pos, i] = $"Species: {species}\n";
                 break;
             case 2:
                 Console.WriteLine("Enter age.");
                 age = Console.ReadLine();
-                ourAnimals[pos, i] = $"Age:\n{age}\n";
+                ourAnimals[pos, i] = $"Age: {age}\n";
                 break;
             case 3:
                 Console.WriteLine("Enter condition description.");
                 condition = Console.ReadLine();
-                ourAnimals[pos, i] = $"Condition:\n{condition}\n";
+                ourAnimals[pos, i] = $"Physical description: {condition}\n";
                 break;
             case 4:
                 Console.WriteLine("Enter personality description.");
                 personality = Console.ReadLine();
-                ourAnimals[pos, i] = $"Personality:\n{personality}\n";
+                ourAnimals[pos, i] = $"Personality: {personality}\n";
                 break;
             case 5:
                 Console.WriteLine("Enter nickname.");
                 nickname = Console.ReadLine();
-                ourAnimals[pos, i] = $"Nickname:\n{nickname}\n";
+                ourAnimals[pos, i] = $"Nickname: {nickname}\n";
+                break;
+            case 6:
+                Console.WriteLine("Enter suggested donation.");
+                suggestedDonation = Console.ReadLine();
+                validateDonation(suggestedDonation);
+                ourAnimals[pos, i] = $"Suggested donation: {suggestedDonation:C}\n";
                 break;
         }
     }
 }
 
 
+// Loop controlling if valid values were entered.
 bool message_loop()
 {
     string decision = "";
@@ -256,7 +306,7 @@ bool message_loop()
         {
             case ("yes" or "y"):
                 return true;
-            case ("no" or "n"):
+            case ("no" or "n" or "not"):
                 Console.WriteLine("Enter yes or not.");
                 return false;
             default:
@@ -272,6 +322,7 @@ bool message_loop()
 int sanitize_pos_input(string pos)
 {
     int int_pos = -1;
+    
     try
     {
         int_pos = Convert.ToInt32(pos);
@@ -290,8 +341,8 @@ int sanitize_pos_input(string pos)
 // if entry is not filled writes message and returns false, otherwise only returns true
 bool is_entry_filled(int pos)
 {
-    if (ourAnimals[pos, 0] != null && ourAnimals[pos, 0].Length > 0) return true;
-    Console.WriteLine("Entry is not valid because it doesn't correspond to animal.\n");
+    if (ourAnimals[pos, 0] != null && ourAnimals[pos, 0].Length > 6) return true;
+    Console.WriteLine("Entry is empty or not filled correctly.\n");
     return false;
 }
 
@@ -321,6 +372,16 @@ int choose_entry(bool pass)
 }
 
 
+// Casts donation to decimal and back.
+bool validateDonation(string donation)
+{
+    decimal result = 1.00m;
+    bool truth = decimal.TryParse(donation, out result);
+    Console.WriteLine(truth);
+    return truth;
+}
+
+
 // Allows to edit specified animal age.
 void edit_age()
 {
@@ -343,12 +404,16 @@ void edit_pers()
 {
     int int_pos = choose_entry(false);
     string inp_pers = "";
-
-    do
+    bool overwrite = overwrite_entry(int_pos);
+    if (overwrite)
     {
-        Console.WriteLine($"Provide new personality description.\n");
-        inp_pers = Console.ReadLine();
-    } while (inp_pers.Length <= 0);
+        do
+        {
+            Console.WriteLine($"Provide new personality description.\n");
+            inp_pers = Console.ReadLine();
+        } while (inp_pers.Length <= 0);
+    }
+    else return;
     
     ourAnimals[int_pos, 5] = inp_pers;
     Console.WriteLine($"Personality description changed to {inp_pers}.\n");
@@ -363,7 +428,7 @@ void are_nick_pers_compl()
     string nickname = ourAnimals[int_pos, 5];
     Console.WriteLine($"Personality description: {personality}\nNickname: {nickname}.\n");
 
-    if (personality.Length >= 3 && nickname.Length > 1) Console.WriteLine("Entries are filled.\n");
+    if (personality.Length >= 3 && nickname.Length > 1) Console.WriteLine("Entries are filled correctly.\n");
     
     else Console.WriteLine("Entries are not filled correctly.\n");
 }
